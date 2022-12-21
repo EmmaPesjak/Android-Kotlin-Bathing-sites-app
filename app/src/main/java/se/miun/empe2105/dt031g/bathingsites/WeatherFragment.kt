0fc897f932ff21b3334e1a7255e80aa19148d731
Emma Pesjak <empe2105@student.miun.se>
2022-12-21 16:54:41 +0100

    just need to finetune and validate user input now

--- app/src/main/java/se/miun/empe2105/dt031g/bathingsites/WeatherFragment.kt
+++ app/src/main/java/se/miun/empe2105/dt031g/bathingsites/WeatherFragment.kt
@@ -2,6 +2,7 @@ package se.miun.empe2105.dt031g.bathingsites
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.content.DialogInterface
 import android.graphics.drawable.Drawable
 import android.os.Bundle
@@ -13,35 +14,15 @@ import androidx.fragment.app.Fragment
 import kotlinx.coroutines.*
 import org.json.JSONException
 import org.json.JSONObject
-import java.io.InputStream
 import java.net.URL
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
- * Use the [WeatherFragment.newInstance] factory method to
- * create an instance of this fragment.
  */
 class WeatherFragment : DialogFragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    //använder ej?
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     //Denna behövs
     override fun onCreateView(
@@ -56,91 +37,12 @@ class WeatherFragment : DialogFragment() {
 
     //https://stackoverflow.com/questions/18601049/adding-positive-negative-button-to-dialogfragments-dialog
 
-
-//    companion object {
-//        var weatherIsDownloaded = false
-//    }
-//
-//    // fast måste ha någon load först
-//    @OptIn(DelicateCoroutinesApi::class)
-//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-//
-//        if (weatherIsDownloaded) {
-//            return getWeatherDialog()
-//        } else {
-//
-//            //executor istället??
-////            runBlocking {
-////                GlobalScope.launch {
-////                    delay(5000)
-////                }
-////            }
-//
-//            //https://www.baeldung.com/kotlin/create-thread-pool
-//            val service: ExecutorService = Executors.newSingleThreadExecutor();
-//            println("I am working hard")
-//            service.submit {
-//                Thread.sleep(5000) // Imitate slow IO
-//                println("I am reporting on the progress")
-//
-//                //ladda ner möget
-//
-//                isDismiss()
-//                println("would like to show new dialog now")
-//
-//                //problemet här är väl att dialogen inte visas i rätt activity...
-//                // sätta allt utanför detta?
-//                getWeatherDialog().show()
-//            }
-//            println("Meanwhile I continue to work")
-//
-////            service.execute(Runnable {
-////                override fun run() {
-////
-////                }
-////            })
-//
-//            //fixa om man stänger ner appen??
-//
-//            // glöm inte permissions
-//
-//
-//            //får kanske kolla på riktigt om den fins i internal storage
-//            //för nu laddar den ner varje gång man startar om
-//            weatherIsDownloaded = true
-//
-//
-//            val inflater = activity?.layoutInflater
-//
-//            val dialogView = inflater?.inflate(R.layout.fragment_weather, null)
-//            val builder = AlertDialog.Builder(activity)
-//
-//            builder.setView(dialogView)
-//            isdialog = builder.create()
-//
-//            return isdialog
-//
-////            return AlertDialog.Builder(activity)
-////                .setMessage(R.string.getting_current_weather)
-////                .create()
-//
-//        }
-//
-//        //här ska nerladdningen statas? (men kolla om det redan fnns nerladdat väder så man kan gå in och ut ut rutan
-//        // utan att ladda ner igen) och så ska dialogen visa en progressbar,
-//        //sen ska detta visas: fast med väder
-//
-//    }
-
-
     private lateinit var alertDialog : AlertDialog
-    //private lateinit var activityAdd : Activity
+    private lateinit var progressDialog: ProgressDialog
 
 
     //ladda alltid ner nytt väder eftersom det kan ändras.
     fun createDialog(activity: Activity, address: String? = "", longitude: Int? = null, latitude: Int? = null) {
-
-        //activityAdd = activity  //detta kan raderas? använder bara den här
 
 
         //https://www.baeldung.com/kotlin/create-thread-pool
@@ -177,46 +79,45 @@ class WeatherFragment : DialogFragment() {
                 getWeatherData(completeUrl)
 
 
-
-
-
-
                 // måste fixa något ifall staden/koordinaterna inte fungerar
             } else {
                 println("Should never occur")
                 // måste få antingen adress eller coordinater så borde inte behöve göra mer här?
             }
 
-            dismissDialog()
-
-            //får kanske kolla på riktigt om den fins i internal storage
-            //för nu laddar den ner varje gång man startar om
-
+            //alertDialog.dismiss()
+            progressDialog.dismiss()
         }
 
+        val message = activity.getString(R.string.getting_current_weather)
 
-        //fixa om man stänger ner appen??
-
-        // ska den hämtade datan sparas? kan den laddas ner på nytt varje gång? ska nerladdningen raderas?
+        progressDialog = ProgressDialog(activity)
+        progressDialog.setMessage(message)
+        progressDialog.setCancelable(false)
+        progressDialog.setOnDismissListener {
+            showWeatherDialog(activity)
+        }
+        progressDialog.show()
 
 
         //inflatea fragmentet med progressbaren
-        val inflater = activity.layoutInflater
-        val dialogView = inflater.inflate(R.layout.fragment_weather, null)
-        val builder = AlertDialog.Builder(activity)
-        builder.setView(dialogView)
-        alertDialog = builder.create()
-
-        //https://medium.com/@stlin813/sequentially-show-multiple-dialogs-in-rxjava-5b57b2b9595b
-        //sätt en listener så vädret kan visas sen
-        alertDialog.setOnDismissListener(DialogInterface.OnDismissListener {
-            showWeatherDialog(activity)
-        })
-
-        //visa dialogen
-        alertDialog.show()
+//        val inflater = activity.layoutInflater
+//        val dialogView = inflater.inflate(R.layout.fragment_weather, null)
+//        val builder = AlertDialog.Builder(activity)
+//        builder.setView(dialogView)
+//        alertDialog = builder.create()
+//
+//        //https://medium.com/@stlin813/sequentially-show-multiple-dialogs-in-rxjava-5b57b2b9595b
+//        //sätt en listener så vädret kan visas sen
+//        alertDialog.setOnDismissListener(DialogInterface.OnDismissListener {
+//            showWeatherDialog(activity)
+//        })
+//
+//        //visa dialogen
+//        alertDialog.show()
 
     }
+
 
     //https://stackoverflow.com/questions/44883593/how-to-read-json-from-url-using-kotlin-android
     fun parseResponse(json: String): JSONObject? {
@@ -229,59 +130,45 @@ class WeatherFragment : DialogFragment() {
         return jsonObject
     }
 
-
     //använder inte inputstream här som i tipset
     fun getWeatherData(url: String) {
         val apiResponse = URL(url).readText()
 
         val jsonObject = parseResponse(apiResponse)
 
+        //https://johncodeos.com/how-to-parse-json-in-android-using-kotlin/
         val weatherJsonArray = jsonObject?.getJSONArray("weather")
         val icon = weatherJsonArray?.getJSONObject(0)?.getString("icon")
-        println("Icon:  $icon")
-        val description = weatherJsonArray?.getJSONObject(0)?.getString("description")
-        println("Description:  $description")
-
-        weatherDescription = description.toString()
-
-
+        weatherDescription = weatherJsonArray?.getJSONObject(0)?.getString("description").toString()
         val mainJsonObj = jsonObject?.getJSONObject("main")
-        val temp = mainJsonObj?.get("temp")
-        println("Temp: $temp")
-        temperature = temp.toString()
+        temperature = mainJsonObj?.get("temp").toString()
 
-        val coordJsonObj = jsonObject?.getJSONObject("coord")
-        val lat = coordJsonObj?.get("lat")
-        println("Lat: $lat")
-        val longi = coordJsonObj?.get("lon")
-        println("Long: $longi")
 
-        val source = "https://openweathermap.org/img/w/$icon.png"
-
-        val inputStream: InputStream = requireContext().assets.open(source)
-        val d = Drawable.createFromStream(inputStream, null)
-        d.setBounds(0, 0, d.intrinsicWidth, d.intrinsicHeight)
-        weatherIcon = d
+        // IN MED DETTA I STRINGS ELLER NÅGOT
+        val imgUrl = "https://openweathermap.org/img/w/$icon.png"
+        val stream = URL(imgUrl).openStream()
+        val drawable = Drawable.createFromStream(stream, "src")
+        weatherIcon = drawable
     }
 
     lateinit var temperature: String  //avrunda?
     lateinit var weatherDescription: String
-    lateinit var weatherIcon: Drawable
-
-    private fun dismissDialog() {
-        alertDialog.dismiss()
-    }
+    private lateinit var weatherIcon : Drawable
 
     private fun showWeatherDialog(activity: Activity) {
 
         //https://stackoverflow.com/questions/18601049/adding-positive-negative-button-to-dialogfragments-dialog
+        //https://stackoverflow.com/questions/36699987/show-degree-symbol-in-a-textview
 
-        val builder = AlertDialog.Builder(activity)
+
+        //gör om xmlen och inflatea så det ser ordentligt ut
+
+        AlertDialog.Builder(activity)
             .setTitle(R.string.current_weather)
-            .setMessage("$weatherIcon $weatherDescription \n$temperature\u2103") //hårdkoda inte? in i strings?
+            .setMessage("$weatherDescription \n$temperature\u2103") //hårdkoda inte? in i strings?
+            .setIcon(weatherIcon)
             .setNegativeButton(R.string.ok
             ) { dialog, _ -> dialog.dismiss() }
             .create().show()
-
     }
 }
