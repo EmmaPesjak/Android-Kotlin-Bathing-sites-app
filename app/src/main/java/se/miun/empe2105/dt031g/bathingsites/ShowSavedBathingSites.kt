d70d138ebf5aa6050987c5dd7e124fac380b26ea
Emma Pesjak <empe2105@student.miun.se>
2022-12-23 15:44:28 +0100

    still need to fix recycler

--- app/src/main/java/se/miun/empe2105/dt031g/bathingsites/ShowSavedBathingSites.kt
+++ app/src/main/java/se/miun/empe2105/dt031g/bathingsites/ShowSavedBathingSites.kt
@@ -1,11 +1,15 @@
 package se.miun.empe2105.dt031g.bathingsites
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+
+
+//https://www.youtube.com/watch?v=-dm4cYlKiAA
 
 class ShowSavedBathingSites : AppCompatActivity() {
 
@@ -17,24 +21,42 @@ class ShowSavedBathingSites : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_saved_bathing_sites)
         appDatabase = AppDatabase.getDatabase(this)
+
+
+        val textview = findViewById<TextView>(R.id.will_be_recycler)
+
+        readData(textview)
     }
 
 
-    private fun readData() {
+    private fun readData(textView: TextView) {
 
         lateinit var bathingSites: List<SavedBathingSite>
 
         GlobalScope.launch {
             bathingSites = appDatabase.bathingSiteDao().getAllNames()
-            displayData(bathingSites)
+            displayData(bathingSites, textView)
         }
     }
 
 
-    private suspend fun displayData(bathingSites: List<SavedBathingSite>) {
+    private suspend fun displayData(bathingSites: List<SavedBathingSite>, textView: TextView) {
 
         withContext(Dispatchers.Main) {
-            //displaya
+            //displaya men detta ska ju in i en recycler :)
+
+
+
+            val stringBuilder = StringBuilder()
+
+
+            for (i in bathingSites) {
+                stringBuilder.append(i.name + " ")
+            }
+
+            val message = stringBuilder.toString()
+
+            textView.text = message
         }
     }
 }
